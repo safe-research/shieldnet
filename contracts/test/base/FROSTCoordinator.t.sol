96c43eb47bc9ac0447acf2efa200b48693ac440e
Nicholas Rodrigues Lordello <n@lordello.net>
2025-11-05 08:11:56 +0100

    one more typo

--- contracts/test/base/FROSTCoordinator.t.sol
+++ contracts/test/base/FROSTCoordinator.t.sol
@@ -58,11 +58,11 @@ contract FROSTCoordinatorTest is Test {
         // Round 1.3
         // Note that `cc[index]` is equivalent to `commitments[index].c`. We
         // need the additional array to keep `ForgeSecp256k1.P` versions of our
-        // points, because implementing elliptic curve addition natively on the
-        // EVM is prohibitively slow, and so we need to use the built-in Forge
-        // cheatcodes for doing the elliptic curve operations for the test.
-        // These elliptic curve operations are done offchain anyway, so this
-        // is not a concern for the actual production system.
+        // points, because implementing elliptic curve multiplication natively
+        // on the EVM is prohibitively slow, and so we need to use the built-in
+        // Forge cheatcodes for doing the elliptic curve operations for the
+        // test. These elliptic curve operations are done offchain anyway, so
+        // this is not a concern for the actual production system.
         ForgeSecp256k1.P[][] memory cc = new ForgeSecp256k1.P[][](COUNT + 1);
         for (uint256 index = 1; index <= COUNT; index++) {
             FROSTCoordinator.KeyGenCommitment memory commitment = commitments[index];
