66f19fc6992d1055694a6d27673ffafd3c37f41d
Nicholas Rodrigues Lordello <n@lordello.net>
2025-12-12 11:49:30 +0100

    Use Logging Framework (#74)
    This PR adds a logging framework to the project. I chose between
    `winston` and `pino`, by far the two most recommended ones on Reddit.
    
    I ended up going with `winston`, even if I had originally opted for
    `pino` (as it was smaller, faster, and more widely used according to
    NPM.js statistics) because:
    - `pino` uses `WebWorker` API to send all logging to a separate thread
    to ensure it doesn't impact the app's performance. This has some
    downsides that affected how we use logging. In particular, not all
    options are serializable which requires a custom transport [1] to
    correctly configure, which doesn't work well with TypeScript [2].
    - `pino-pretty`, which we used to make the logging output look nice when
    connected to a TTY, did not work with `vitest`; logs were not appearing
    in `stdout`, and I believe it is something related to how `vitest`
    captures all `stdout` and how `pino-pretty` uses stdout redirection [3].
    
    In the end, `winston` just worked, had a much simpler setup, and I don't
    think logging performance impact is an issue for us.
    
    [1]:
    <https://github.com/pinojs/pino-pretty?tab=readme-ov-file#handling-non-serializable-options>
    [2]: <https://github.com/pinojs/pino/issues/2203>
    [3]: <https://github.com/pinojs/pino-pretty#limitations>

--- validator/src/__tests__/config.ts
+++ validator/src/__tests__/config.ts
@@ -3,13 +3,17 @@ import { InMemoryClientStorage } from "../consensus/storage/inmemory.js";
 import { SqliteClientStorage } from "../consensus/storage/sqlite.js";
 import { InMemoryStateStorage } from "../machine/storage/inmemory.js";
 import { SqliteStateStorage } from "../machine/storage/sqlite.js";
+import { createLogger } from "../utils/logging.js";
 
 const { SHIELDNET_TEST_VERBOSE, SHIELDNET_TEST_STORAGE } = process.env;
 
-export const log =
-	SHIELDNET_TEST_VERBOSE === "true" || SHIELDNET_TEST_VERBOSE === "1"
-		? (...args: unknown[]) => console.log(...args)
-		: (..._args: unknown[]) => {};
+export const silentLogger = createLogger({ level: "silent" });
+export const testLogger = createLogger({
+	level: SHIELDNET_TEST_VERBOSE === "true" || SHIELDNET_TEST_VERBOSE === "1" ? "debug" : "silent",
+	pretty: true,
+});
+
+export const log = testLogger.debug.bind(testLogger);
 
 export const createClientStorage =
 	SHIELDNET_TEST_STORAGE === "sqlite"
--- validator/src/utils/logging.ts
+++ validator/src/utils/logging.ts
@@ -1,5 +1,39 @@
-export type Logger = {
-	error(error: Error): void;
-	debug(msg: unknown): void;
-	info(msg: unknown): void;
+import util from "node:util";
+import winston, { type Logger } from "winston";
+
+export type { Logger } from "winston";
+
+export type LoggingOptions = {
+	level?: "error" | "warn" | "info" | "debug" | "silent";
+	pretty?: boolean;
+};
+
+const SPLAT = Symbol.for("splat");
+const prettyFormat = winston.format.printf(({ timestamp, level, message, [SPLAT]: splat }) => {
+	// We need to turn the "splat", i.e. the extra parameters passed to the log
+	// after the message, into an array for inspecting. AFAICT, `winston` always
+	// gives us an `array | undefined`, but the type-system says otherwise so
+	// handle as following:
+	// - If we have an array, then "yay"
+	// - If we have some other value, then arrayify it
+	// - If we have undefined, then there are no additional args
+	const args = Array.isArray(splat) ? splat : splat !== undefined ? [splat] : [];
+
+	const text = [message, ...args]
+		.map((part) => (typeof part === "string" ? part : util.inspect(part, { colors: true })))
+		.join(" ");
+	return `[${timestamp} ${level}]: ${text}`;
+});
+
+export const createLogger = (options: LoggingOptions): Logger => {
+	const level = options.level === "silent" ? { silent: true } : { level: options.level ?? "info" };
+	const format =
+		options.pretty === true
+			? winston.format.combine(winston.format.timestamp(), winston.format.colorize(), prettyFormat)
+			: winston.format.json();
+	return winston.createLogger({
+		...level,
+		format,
+		transports: [new winston.transports.Console()],
+	});
 };
