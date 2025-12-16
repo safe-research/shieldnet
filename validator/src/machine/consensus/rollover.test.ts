b1733e3c561e71ebe911f757a52f96a94fca2ea6
Richard Meissner <rmeissner@users.noreply.github.com>
2025-12-16 11:07:46 +0100

    Add tests for rollover check (#76)

---
+++ validator/src/machine/consensus/rollover.test.ts
@@ -0,0 +1,248 @@
+import { ethAddress, zeroAddress, zeroHash } from "viem";
+import { describe, expect, it, vi } from "vitest";
+import type { KeyGenClient } from "../../consensus/keyGen/client.js";
+import type { ShieldnetProtocol } from "../../consensus/protocol/types.js";
+import { toPoint } from "../../frost/math.js";
+import type { FrostPoint } from "../../frost/types.js";
+import type { ConsensusState, MachineConfig, MachineStates } from "../types.js";
+import { checkEpochRollover } from "./rollover.js";
+
+// --- Test Data ---
+const TEST_POINT: FrostPoint = toPoint({
+	x: 73844941487532555987364396775795076447946974313865618280135872376303125438365n,
+	y: 29462187596282402403443212507099371496473451788807502182979305411073244917417n,
+});
+
+const MACHINE_CONFIG: MachineConfig = {
+	defaultParticipants: [
+		{
+			id: 1n,
+			address: zeroAddress,
+		},
+		{
+			id: 3n,
+			address: zeroAddress,
+		},
+		{
+			id: 7n,
+			address: zeroAddress,
+		},
+	],
+	genesisSalt: zeroHash,
+	keyGenTimeout: 20n,
+	signingTimeout: 0n,
+	blocksPerEpoch: 10n,
+};
+
+const CONSENSUS_STATE: ConsensusState = {
+	activeEpoch: 0n,
+	stagedEpoch: 0n,
+	genesisGroupId: "0x5af35afe",
+	groupPendingNonces: {},
+	epochGroups: {},
+	signatureIdToMessage: {},
+};
+
+// By default we setup in a genesis state
+// This avoids that nonce commitments are triggered every time
+const MACHINE_STATES: MachineStates = {
+	rollover: {
+		id: "waiting_for_rollover",
+	},
+	signing: {},
+};
+
+// --- Tests ---
+describe("check rollover", () => {
+	it("should not trigger key gen in genesis state", async () => {
+		const protocol = {} as unknown as ShieldnetProtocol;
+		const keyGenClient = {} as unknown as KeyGenClient;
+		const consensus: ConsensusState = {
+			...CONSENSUS_STATE,
+			genesisGroupId: undefined,
+		};
+		const diff = checkEpochRollover(MACHINE_CONFIG, protocol, keyGenClient, consensus, MACHINE_STATES, 1n);
+
+		expect(diff).toStrictEqual({});
+	});
+
+	it("should not trigger key gen if not waiting for rollover", async () => {
+		const protocol = {} as unknown as ShieldnetProtocol;
+		const keyGenClient = {} as unknown as KeyGenClient;
+		const machineStates: MachineStates = {
+			...MACHINE_STATES,
+			rollover: {
+				id: "collecting_commitments",
+				groupId: "0xda5afe3",
+				nextEpoch: 2n,
+				deadline: 22n,
+			},
+		};
+		const diff = checkEpochRollover(MACHINE_CONFIG, protocol, keyGenClient, CONSENSUS_STATE, machineStates, 1n);
+
+		expect(diff).toStrictEqual({});
+	});
+
+	it("should not trigger key gen if rollover is staged", async () => {
+		const protocol = {} as unknown as ShieldnetProtocol;
+		const keyGenClient = {} as unknown as KeyGenClient;
+		const consensus: ConsensusState = {
+			...CONSENSUS_STATE,
+			stagedEpoch: 1n,
+		};
+		const diff = checkEpochRollover(MACHINE_CONFIG, protocol, keyGenClient, consensus, MACHINE_STATES, 1n);
+
+		expect(diff).toStrictEqual({});
+	});
+
+	it("should update staged rollover without triggering key gen", async () => {
+		const protocol = {} as unknown as ShieldnetProtocol;
+		const keyGenClient = {} as unknown as KeyGenClient;
+		const machineStates: MachineStates = {
+			...MACHINE_STATES,
+			rollover: {
+				id: "collecting_commitments",
+				groupId: "0xda5afe3",
+				nextEpoch: 2n,
+				deadline: 22n,
+			},
+		};
+		const consensus: ConsensusState = {
+			...CONSENSUS_STATE,
+			stagedEpoch: 1n,
+		};
+		const diff = checkEpochRollover(MACHINE_CONFIG, protocol, keyGenClient, consensus, machineStates, 10n);
+
+		expect(diff.actions).toBeUndefined();
+		expect(diff.rollover).toBeUndefined();
+		expect(diff.consensus).toStrictEqual({
+			activeEpoch: 1n,
+			stagedEpoch: 0n,
+		});
+		expect(diff.signing).toBeUndefined();
+	});
+
+	it("should trigger key gen if no key gen is active", async () => {
+		const consensus = vi.fn();
+		consensus.mockReturnValueOnce(ethAddress);
+		const protocol = {
+			consensus,
+		} as unknown as ShieldnetProtocol;
+		const setupGroup = vi.fn();
+		const groupSetup = {
+			groupId: "0x5afe02",
+			participantsRoot: "0x5afe5afe5afe",
+			participantId: 3n,
+			commitments: [TEST_POINT],
+			pok: {
+				r: TEST_POINT,
+				mu: 100n,
+			},
+			poap: ["0x5afe5afe5afe01"],
+		};
+		setupGroup.mockReturnValueOnce(groupSetup);
+		const keyGenClient = {
+			setupGroup,
+		} as unknown as KeyGenClient;
+		const diff = checkEpochRollover(MACHINE_CONFIG, protocol, keyGenClient, CONSENSUS_STATE, MACHINE_STATES, 10n);
+
+		expect(diff.actions).toStrictEqual([
+			{
+				id: "key_gen_start",
+				participants: groupSetup.participantsRoot,
+				count: 3n,
+				threshold: 2n,
+				context: "0x00000000eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee0000000000000002",
+				participantId: 3n,
+				commitments: groupSetup.commitments,
+				pok: groupSetup.pok,
+				poap: groupSetup.poap,
+			},
+		]);
+		expect(diff.rollover).toStrictEqual({
+			id: "collecting_commitments",
+			groupId: "0x5afe02",
+			nextEpoch: 2n,
+			deadline: 30n,
+		});
+		expect(diff.consensus).toStrictEqual({
+			epochGroup: [2n, { groupId: "0x5afe02", participantId: 3n }],
+			activeEpoch: 0n,
+			stagedEpoch: 0n,
+		});
+		expect(diff.signing).toBeUndefined();
+
+		expect(consensus).toBeCalledTimes(1);
+		expect(setupGroup).toBeCalledTimes(1);
+		expect(setupGroup).toBeCalledWith(
+			MACHINE_CONFIG.defaultParticipants,
+			3n,
+			2n,
+			"0x00000000eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee0000000000000002",
+		);
+	});
+
+	it("should trigger key gen after rollover if no key gen is active", async () => {
+		const consensus = vi.fn();
+		consensus.mockReturnValueOnce(ethAddress);
+		const protocol = {
+			consensus,
+		} as unknown as ShieldnetProtocol;
+		const setupGroup = vi.fn();
+		const groupSetup = {
+			groupId: "0x5afe02",
+			participantsRoot: "0x5afe5afe5afe",
+			participantId: 3n,
+			commitments: [TEST_POINT],
+			pok: {
+				r: TEST_POINT,
+				mu: 100n,
+			},
+			poap: ["0x5afe5afe5afe01"],
+		};
+		setupGroup.mockReturnValueOnce(groupSetup);
+		const keyGenClient = {
+			setupGroup,
+		} as unknown as KeyGenClient;
+		const consensusState: ConsensusState = {
+			...CONSENSUS_STATE,
+			stagedEpoch: 1n,
+		};
+		const diff = checkEpochRollover(MACHINE_CONFIG, protocol, keyGenClient, consensusState, MACHINE_STATES, 10n);
+
+		expect(diff.actions).toStrictEqual([
+			{
+				id: "key_gen_start",
+				participants: groupSetup.participantsRoot,
+				count: 3n,
+				threshold: 2n,
+				context: "0x00000000eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee0000000000000002",
+				participantId: 3n,
+				commitments: groupSetup.commitments,
+				pok: groupSetup.pok,
+				poap: groupSetup.poap,
+			},
+		]);
+		expect(diff.rollover).toStrictEqual({
+			id: "collecting_commitments",
+			groupId: "0x5afe02",
+			nextEpoch: 2n,
+			deadline: 30n,
+		});
+		expect(diff.consensus).toStrictEqual({
+			epochGroup: [2n, { groupId: "0x5afe02", participantId: 3n }],
+			activeEpoch: 1n,
+			stagedEpoch: 0n,
+		});
+		expect(diff.signing).toBeUndefined();
+
+		expect(consensus).toBeCalledTimes(1);
+		expect(setupGroup).toBeCalledTimes(1);
+		expect(setupGroup).toBeCalledWith(
+			MACHINE_CONFIG.defaultParticipants,
+			3n,
+			2n,
+			"0x00000000eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee0000000000000002",
+		);
+	});
+});
