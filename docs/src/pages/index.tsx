c8b0c367ab448cb4689f01c074b896d7c1519045
Tobias Schubotz <1380529+tschubotz@users.noreply.github.com>
2026-01-08 13:16:54 +0100

    Formatting

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,40 +1,33 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Heading from '@theme/Heading';
-import styles from './index.module.css';
+import Heading from "@theme/Heading";
+import Layout from "@theme/Layout";
+import clsx from "clsx";
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          Shieldnet
-        </Heading>
-        <p className="hero__subtitle">
-          Shieldnet enforces transaction security onchain - protecting users from high-risk threats
-        </p>
-        <div className={styles.buttons}>
-          <a
-            className="button button--secondary button--lg"
-            href="/docs/introduction">
-            Get Started
-          </a>
-        </div>
-      </div>
-    </header>
-  );
+	return (
+		<header className={clsx("hero hero--primary", styles.heroBanner)}>
+			<div className="container">
+				<Heading as="h1" className="hero__title">
+					Shieldnet
+				</Heading>
+				<p className="hero__subtitle">
+					Shieldnet enforces transaction security onchain - protecting users from high-risk threats
+				</p>
+				<div className={styles.buttons}>
+					<a className="button button--secondary button--lg" href="/docs/introduction">
+						Get Started
+					</a>
+				</div>
+			</div>
+		</header>
+	);
 }
 
 export default function Home(): JSX.Element {
-  return (
-    <Layout
-      title="Shieldnet"
-      description="Shieldnet enforces transaction security onchain">
-      <HomepageHeader />
-      <main>
-        {/* TODO */}
-      </main>
-    </Layout>
-  );
+	return (
+		<Layout title="Shieldnet" description="Shieldnet enforces transaction security onchain">
+			<HomepageHeader />
+			<main>{/* TODO */}</main>
+		</Layout>
+	);
 }
