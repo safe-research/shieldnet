cb133bf5015d350a848122ca92f6c209718239bc
Tobias Schubotz <1380529+tschubotz@users.noreply.github.com>
2026-01-08 13:55:58 +0100

    Remore .docusaurus chache folder

--- docs/.docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/workspaces/shieldnet/docs/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/workspaces/shieldnet/node_modules/infima/dist/css/default/default.css"),
-  require("/workspaces/shieldnet/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/workspaces/shieldnet/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/workspaces/shieldnet/docs/src/css/custom.css"),
-];
