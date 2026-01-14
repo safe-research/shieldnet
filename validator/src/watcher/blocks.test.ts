5c1b068b070043ba5b78619803530e33aab993d2
Nicholas Rodrigues Lordello <n@lordello.net>
2026-01-14 11:29:50 +0100

    Introduce Block Intexer (#121)
    This PR introduces a block indexer that (hopefully - fingers crossed)
    produces records for:
    
    - "Warping" to a particular block; this record is produced when moving
    historic blocks (i.e. not within the maximum reorg range) and allows the
    event indexing (which will be added later) to use ranged `eth_getLogs`
    queries safely. This is done as a startup optimization, when the
    validator restarts it can catch up to the current block with fewer node
    round-trips.
    - "Uncling" a block and its children, which happens when the indexer
    detects a reorg.
    - "New" block, with a hash and logs bloom filter (to do some client
    filtering before deciding whether or not it needs to get logs).
    
    We also have some tricks to try and reduce the total number of RPC
    queries needed for the indexer:
    
    - We estimate when the next block is likely to be ready, and sleep until
    then before attempting an `eth_getBlock` call.
    - We retry with small sleep increments to account for small delays in
    block propagation
    - If we detect that a block is not likely to come, then we assume that
    slot was skipped (which happens fairly often in low-traffic networks)
    and wait until the next slot + propagation delay before retrying again.
    
    Additionally, I added some unit tests to verify some of the logic (and
    should have pretty good coverage).

---
+++ validator/src/watcher/blocks.test.ts
@@ -0,0 +1,287 @@
+import { type Block, BlockNotFoundError, type GetBlockParameters, keccak256, numberToHex, toHex } from "viem";
+import { describe, expect, it, vi } from "vitest";
+
+import { BlockWatcher, type Client, type Timer } from "./blocks.js";
+
+const CONFIG = {
+	blockTime: 2000,
+	maxReorgDepth: 2,
+	blockPropagationDelay: 500,
+	blockRetryDelays: [200, 100, 50],
+};
+
+const setupCreate = (config: { lastIndexedBlock: bigint | null; maxReorgDepth?: number }) => {
+	const getBlock = vi.fn();
+	const now = vi.fn();
+	const sleep = vi.fn();
+
+	return {
+		create() {
+			return BlockWatcher.create({
+				...CONFIG,
+				...config,
+				client: {
+					getBlock,
+				} as unknown as Client,
+				timer: {
+					now,
+					sleep,
+				} as unknown as Timer,
+			});
+		},
+		mocks: {
+			getBlock,
+			now,
+			sleep,
+		},
+	};
+};
+
+const setupNext = async (config: { latestBlock: bigint; startTime: number; maxReorgDepth?: number }) => {
+	const { create, mocks } = setupCreate({
+		lastIndexedBlock: null,
+		...config,
+	});
+
+	let created: BlockWatcher | null = null;
+	await mocks.getBlock.withImplementation(
+		({ blockTag, blockNumber }: GetBlockParameters) => {
+			if (blockTag === "latest") {
+				return block({ number: 1000n });
+			}
+			if (blockNumber !== undefined) {
+				return block({ number: blockNumber });
+			}
+			throw new Error("unexpected eth_getBlock parameters");
+		},
+		async () => {
+			created = await create();
+		},
+	);
+	mocks.getBlock.mockClear();
+
+	// We have to trick the type-system here, it doesn't understand that the callback in the
+	// `withImplementation` call can change the value and thinks `created: null`.
+	const blocks = created as unknown as BlockWatcher;
+
+	// Skip the queued records.
+	blocks.queued();
+
+	// Setup useful default mocks for the timer.
+	let time = config.startTime;
+	mocks.now.mockImplementation(() => time);
+	mocks.sleep.mockImplementation((ms: number) => {
+		time += ms;
+		return Promise.resolve();
+	});
+
+	return {
+		next() {
+			return blocks.next();
+		},
+		mocks,
+	};
+};
+
+const block = (
+	b: {
+		number: bigint;
+	} & Partial<Pick<Block<bigint, false, "latest">, "hash" | "timestamp" | "parentHash" | "logsBloom">>,
+) => ({
+	number: b.number,
+	hash: b.hash ?? numberToHex(b.number, { size: 32 }),
+	timestamp: b.timestamp ?? (b.number * BigInt(CONFIG.blockTime)) / 1000n,
+	parentHash: b.parentHash ?? numberToHex(b.number - 1n, { size: 32 }),
+	logsBloom: b.logsBloom ?? numberToHex(b.number, { size: 512 }),
+});
+
+const newBlockRecord = (
+	b: {
+		number: bigint;
+	} & Partial<Pick<Block<bigint, false, "latest">, "hash" | "logsBloom">>,
+) => ({
+	type: "record_new_block",
+	blockNumber: b.number,
+	blockHash: b.hash ?? numberToHex(b.number, { size: 32 }),
+	logsBloom: b.logsBloom ?? numberToHex(b.number, { size: 512 }),
+});
+
+describe("BlockWatcher", () => {
+	describe("create", () => {
+		it("initialize a watcher", async () => {
+			const { create, mocks } = setupCreate({ lastIndexedBlock: null });
+
+			mocks.getBlock.mockReturnValueOnce(block({ number: 1000n }));
+			mocks.getBlock.mockReturnValueOnce(block({ number: 999n }));
+
+			const blocks = await create();
+			expect(mocks.getBlock.mock.calls).toEqual([[{ blockTag: "latest" }], [{ blockNumber: 999n }]]);
+
+			expect(blocks.queued()).toStrictEqual([newBlockRecord({ number: 999n }), newBlockRecord({ number: 1000n })]);
+		});
+
+		it("should support continuing from last indexed block", async () => {
+			const { create, mocks } = setupCreate({ lastIndexedBlock: 900n });
+
+			mocks.getBlock.mockReturnValueOnce(block({ number: 1000n }));
+			mocks.getBlock.mockReturnValueOnce(block({ number: 999n }));
+
+			const blocks = await create();
+			expect(mocks.getBlock.mock.calls).toEqual([[{ blockTag: "latest" }], [{ blockNumber: 999n }]]);
+
+			expect(blocks.queued()).toStrictEqual([
+				{
+					type: "record_uncle_block",
+					// Uncling 899, means that we go back to block 898 after starting on 900, which makes
+					// sense given our 2 max reorg depth. I hope there is no off-by-one error!
+					blockNumber: 899n,
+				},
+				{
+					type: "record_warp_to_block",
+					fromBlock: 899n,
+					toBlock: 998n,
+				},
+				newBlockRecord({ number: 999n }),
+				newBlockRecord({ number: 1000n }),
+			]);
+		});
+
+		it("should support no reorg protection", async () => {
+			const { create, mocks } = setupCreate({ lastIndexedBlock: 900n, maxReorgDepth: 0 });
+
+			mocks.getBlock.mockResolvedValueOnce(block({ number: 1000n }));
+
+			const blocks = await create();
+			expect(mocks.getBlock.mock.calls).toEqual([[{ blockTag: "latest" }]]);
+
+			expect(blocks.queued()).toStrictEqual([
+				{
+					type: "record_warp_to_block",
+					fromBlock: 901n,
+					toBlock: 1000n,
+				},
+			]);
+		});
+
+		it("should handle reorgs during initialization", async () => {
+			const { create, mocks } = setupCreate({ lastIndexedBlock: null, maxReorgDepth: 3 });
+
+			mocks.getBlock.mockReturnValueOnce(
+				block({ number: 1000n, hash: keccak256(toHex("bad1000")), parentHash: keccak256(toHex("bad999")) }),
+			);
+			mocks.getBlock.mockReturnValueOnce(block({ number: 998n }));
+			mocks.getBlock.mockReturnValueOnce(
+				block({ number: 999n, hash: keccak256(toHex("bad999")), parentHash: keccak256(toHex("uncle")) }),
+			);
+			mocks.getBlock.mockReturnValueOnce(block({ number: 998n }));
+			mocks.getBlock.mockReturnValueOnce(block({ number: 999n }));
+			mocks.getBlock.mockReturnValueOnce(block({ number: 1000n }));
+
+			const blocks = await create();
+			expect(mocks.getBlock.mock.calls).toEqual([
+				[{ blockTag: "latest" }],
+				[{ blockNumber: 998n }],
+				[{ blockNumber: 999n }],
+				[{ blockNumber: 998n }],
+				[{ blockNumber: 999n }],
+				[{ blockNumber: 1000n }],
+			]);
+
+			expect(blocks.queued()).toStrictEqual([
+				newBlockRecord({ number: 998n }),
+				newBlockRecord({ number: 999n }),
+				newBlockRecord({ number: 1000n }),
+			]);
+		});
+	});
+
+	describe("next", () => {
+		it("wait for the pending block and fetch it", async () => {
+			const { next, mocks } = await setupNext({ latestBlock: 1000n, startTime: 2000100 });
+
+			mocks.getBlock.mockResolvedValueOnce(block({ number: 1001n }));
+
+			const record = await next();
+			expect(mocks.sleep.mock.calls).toEqual([[1900 + 500]]);
+			expect(mocks.getBlock.mock.calls).toEqual([[{ blockNumber: 1001n }]]);
+
+			expect(record).toStrictEqual(newBlockRecord({ number: 1001n }));
+		});
+
+		it("retries if block is not ready when expected", async () => {
+			const { next, mocks } = await setupNext({ latestBlock: 1000n, startTime: 2000100 });
+
+			mocks.getBlock.mockRejectedValueOnce(new BlockNotFoundError({ blockNumber: 1001n }));
+			mocks.getBlock.mockRejectedValueOnce(new BlockNotFoundError({ blockNumber: 1001n }));
+			mocks.getBlock.mockResolvedValueOnce(block({ number: 1001n }));
+
+			const record = await next();
+			expect(mocks.sleep.mock.calls).toEqual([[1900 + 500], [200], [100]]);
+			expect(mocks.getBlock.mock.calls).toEqual([
+				[{ blockNumber: 1001n }],
+				[{ blockNumber: 1001n }],
+				[{ blockNumber: 1001n }],
+			]);
+
+			expect(record).toStrictEqual(newBlockRecord({ number: 1001n }));
+		});
+
+		it("skips slots", async () => {
+			const { next, mocks } = await setupNext({ latestBlock: 1000n, startTime: 2000100 });
+
+			mocks.getBlock.mockRejectedValueOnce(new BlockNotFoundError({ blockNumber: 1001n }));
+			mocks.getBlock.mockRejectedValueOnce(new BlockNotFoundError({ blockNumber: 1001n }));
+			mocks.getBlock.mockRejectedValueOnce(new BlockNotFoundError({ blockNumber: 1001n }));
+			mocks.getBlock.mockRejectedValueOnce(new BlockNotFoundError({ blockNumber: 1001n }));
+			mocks.getBlock.mockResolvedValueOnce(block({ number: 1001n }));
+
+			const record = await next();
+			expect(mocks.sleep.mock.calls).toEqual([[1900 + 500], [200], [100], [50], [1650]]);
+			expect(mocks.getBlock.mock.calls).toEqual([
+				[{ blockNumber: 1001n }],
+				[{ blockNumber: 1001n }],
+				[{ blockNumber: 1001n }],
+				[{ blockNumber: 1001n }],
+				[{ blockNumber: 1001n }],
+			]);
+
+			expect(record).toStrictEqual(newBlockRecord({ number: 1001n }));
+		});
+
+		it("supports deep reorgs", async () => {
+			const { next, mocks } = await setupNext({ latestBlock: 1000n, startTime: 2000100, maxReorgDepth: 5 });
+
+			mocks.getBlock.mockResolvedValueOnce(
+				block({ number: 1001n, hash: keccak256(toHex("reorg1001")), parentHash: keccak256(toHex("reorg1000")) }),
+			);
+			mocks.getBlock.mockResolvedValueOnce(
+				block({ number: 1000n, hash: keccak256(toHex("reorg1000")), parentHash: keccak256(toHex("reorg999")) }),
+			);
+			mocks.getBlock.mockResolvedValueOnce(
+				block({ number: 999n, hash: keccak256(toHex("reorg999")), parentHash: keccak256(toHex("reorg998")) }),
+			);
+			mocks.getBlock.mockResolvedValueOnce(block({ number: 998n, hash: keccak256(toHex("reorg998")) }));
+			mocks.getBlock.mockResolvedValueOnce(
+				block({ number: 999n, hash: keccak256(toHex("reorg999")), parentHash: keccak256(toHex("reorg998")) }),
+			);
+
+			const records = [await next(), await next(), await next(), await next(), await next()];
+			expect(mocks.sleep.mock.calls).toEqual([[1900 + 500]]);
+			expect(mocks.getBlock.mock.calls).toEqual([
+				[{ blockNumber: 1001n }],
+				[{ blockNumber: 1000n }],
+				[{ blockNumber: 999n }],
+				[{ blockNumber: 998n }],
+				[{ blockNumber: 999n }],
+			]);
+
+			expect(records).toStrictEqual([
+				{ type: "record_uncle_block", blockNumber: 1000n },
+				{ type: "record_uncle_block", blockNumber: 999n },
+				{ type: "record_uncle_block", blockNumber: 998n },
+				newBlockRecord({ number: 998n, hash: keccak256(toHex("reorg998")) }),
+				newBlockRecord({ number: 999n, hash: keccak256(toHex("reorg999")) }),
+			]);
+		});
+	});
+});
---
+++ validator/src/watcher/blocks.ts
@@ -0,0 +1,268 @@
+/**
+ * Block watcher.
+ *
+ * This watcher is responsible for reliably producing a stream of blocks. Additionally, it keeps
+ * track of recent block hashes in order to detect reorgs.
+ */
+
+import { BlockNotFoundError, type Hex, type Prettify, type PublicClient, type Block as ViemBlock } from "viem";
+
+export type Client = Pick<PublicClient, "getBlock">;
+export type Timer = {
+	now(): number;
+	sleep(ms: number): Promise<void>;
+};
+
+/**
+ * Required block watcher settings.
+ */
+export type Settings = {
+	blockTime: number;
+	maxReorgDepth: number;
+};
+
+/**
+ * Block watcher options.
+ */
+export type Options = {
+	blockPropagationDelay: number;
+	blockRetryDelays: number[];
+	timer: Timer;
+};
+
+/**
+ * Block watcher creation parameters.
+ */
+export type CreateParams = Prettify<
+	{
+		client: Client;
+		lastIndexedBlock: bigint | null;
+	} & Settings &
+		Partial<Options>
+>;
+
+/**
+ * Block watcher records.
+ */
+export type Record =
+	| { type: "record_warp_to_block"; fromBlock: bigint; toBlock: bigint }
+	| { type: "record_uncle_block"; blockNumber: bigint }
+	| { type: "record_new_block"; blockNumber: bigint; blockHash: Hex; logsBloom: Hex };
+
+type Config = Settings & Options;
+type Block = ViemBlock<bigint, false, "latest">;
+type PendingBlock = {
+	number: bigint;
+	timestampMs: bigint;
+};
+
+export const DEFAULT_OPTIONS = {
+	blockPropagationDelay: 500,
+	blockRetryDelays: [200, 100, 100],
+	timer: {
+		now: Date.now,
+		sleep(ms: number): Promise<void> {
+			return new Promise((resolve) => setTimeout(resolve, ms));
+		},
+	},
+};
+
+export class BlockWatcher {
+	#client: Client;
+	#config: Config;
+	#pending: PendingBlock;
+	#blocks: Block[];
+	#queue: Record[];
+
+	private constructor(client: Client, config: Config) {
+		this.#client = client;
+		this.#config = config;
+		this.#pending = { number: 0n, timestampMs: 0n };
+		this.#blocks = [];
+		this.#queue = [];
+	}
+
+	async #initialize(lastIndexedBlock: bigint | null): Promise<void> {
+		const latest = await this.#client.getBlock({ blockTag: "latest" });
+		const safe = bmax(latest.number - BigInt(this.#config.maxReorgDepth), 0n);
+
+		this.#updateNextPendingBlock(latest);
+
+		if (lastIndexedBlock !== null) {
+			// In order to prevent edge-cases where there is a reorg for a block when the service
+			// was restarted, we always need to create a "fake" reorg `maxReorgDepth` deep in order
+			// to re-index the last blocks before the service shutdown. Queue an watcher record for
+			// uncling the block right after the last safe indexed block.
+			const uncle = bmax(lastIndexedBlock - BigInt(this.#config.maxReorgDepth - 1), 0n);
+			if (uncle <= lastIndexedBlock) {
+				this.#queue.push({ type: "record_uncle_block", blockNumber: uncle });
+			}
+
+			// If possible, add a record warping to the reorg-safe block. This allows optimizations
+			// such as querying logs for block ranges where possible. Note that we cannot warp to
+			// the latest block, as it would be possible to do a `eth_getLogs` query and potentially
+			// retrieve data for an uncled block.
+			if (uncle <= safe) {
+				this.#queue.push({ type: "record_warp_to_block", fromBlock: uncle, toBlock: safe });
+			}
+		}
+
+		// Now query recent blocks, we only need to keep up to `maxReorgDepth` of them for reorg
+		// detection.
+		let parentHash = null;
+		let previouslyQueriedLatest: Block | null = latest;
+		for (let blockNumber = safe + 1n; blockNumber <= latest.number; blockNumber++) {
+			let block: Block;
+			if (previouslyQueriedLatest?.number === blockNumber) {
+				block = previouslyQueriedLatest;
+			} else {
+				block = await this.#client.getBlock({ blockNumber });
+			}
+			if (parentHash === null || parentHash === block.parentHash) {
+				parentHash = block.hash;
+				this.#blocks.push(block);
+			} else {
+				// Under exceptional circumstances, we may observe a reorg while initializing. In
+				// that case, just tear down and start again.
+				parentHash = null;
+				blockNumber = safe;
+				this.#blocks.length = 0;
+
+				// Additionally, we typically make a best effort to re-use the `latest` block query
+				// from the start of the initialization. In case of a detected reorg, we want to
+				// fetch the latest block again, as the one we have is likely uncled.
+				previouslyQueriedLatest = null;
+			}
+		}
+
+		// Queue records for the recent blocks.
+		this.#queue.push(
+			...this.#blocks.map(
+				(block) =>
+					({
+						type: "record_new_block",
+						blockNumber: block.number,
+						blockHash: block.hash,
+						logsBloom: block.logsBloom,
+					}) as const,
+			),
+		);
+	}
+
+	/**
+	 * Updates the pending block for the specified latest block.
+	 */
+	#updateNextPendingBlock({ number, timestamp }: Pick<Block, "number" | "timestamp">) {
+		this.#pending = {
+			number: number + 1n,
+			timestampMs: timestamp * 1000n + BigInt(this.#config.blockTime),
+		};
+	}
+
+	/**
+	 * Sleeps until the pending block is suspected to be ready.
+	 */
+	async #waitForPendingBlock() {
+		const now = this.#config.timer.now();
+		const delay = Number(this.#pending.timestampMs) + this.#config.blockPropagationDelay - now;
+		if (delay > 0) {
+			await this.#config.timer.sleep(delay);
+		}
+	}
+
+	/**
+	 * Consumes all queued events. This allows caller to synchronously process records that are
+	 * already ready from the watcher.
+	 *
+	 * This is currently used for testing.
+	 */
+	public queued(): Record[] {
+		return this.#queue.splice(0, this.#queue.length);
+	}
+
+	/**
+	 * Retrieve the next record from the block watcher.
+	 */
+	public async next(): Promise<Record> {
+		// First, see if we have a queued record that we can return immediately.
+		const queued = this.#queue.shift();
+		if (queued !== undefined) {
+			return queued;
+		}
+
+		// Wait for and retrieve the pending block.
+		let block: Block | null = null;
+		let retryCount = 0;
+		const retryDelays = this.#config.blockRetryDelays;
+		while (block === null) {
+			await this.#waitForPendingBlock();
+			try {
+				block = await this.#client.getBlock({ blockNumber: this.#pending.number });
+			} catch (err) {
+				if (!(err instanceof BlockNotFoundError)) {
+					throw err;
+				}
+
+				// The retry logic is a little weird... but hopefully for a good reason! While we
+				// wait for an expected block timestamp with some propagation delay, it is likely
+				// that the block is either available, or comes shortly after. In this case, we use
+				// the configurable `blockRetryDelays` option in order to retry block queries in
+				// with decreasing delays. However, for chains with little activity (such as Gnosis
+				// Chain), it is not uncommon for blocks to skip slots completely. In order to avoid
+				// us making a lot of successive `getBlock` requests for nothing, we just wait for
+				// the next slot to try again.
+				const index = retryCount++ % (retryDelays.length + 1);
+				if (index < retryDelays.length) {
+					await this.#config.timer.sleep(retryDelays[index]);
+				} else {
+					this.#pending.timestampMs += BigInt(this.#config.blockTime);
+				}
+			}
+		}
+
+		// Now we need to make sure that there are no reorgs!
+		const lastBlock = this.#blocks.at(-1);
+		if (lastBlock !== undefined && lastBlock.hash !== block.parentHash) {
+			this.#blocks.pop();
+			// Note that we update the pending block to be the one that was just uncled, which is
+			// the block immediately before the latest block that we just queried.
+			this.#pending = {
+				number: lastBlock.number,
+				timestampMs: lastBlock.timestamp * 1000n,
+			};
+			return { type: "record_uncle_block", blockNumber: lastBlock.number };
+		}
+
+		// Update our internal accounting:
+		// 1. Add the new block to our blocks list, so that we have the `maxReorgDepth` most recent
+		//    blocks available for reorg detection.
+		// 2. Update the pending block (including the expected mining time, so we know how long we
+		//    should wait before even trying to query a new block).
+		this.#blocks.push(block);
+		while (this.#blocks.length > this.#config.maxReorgDepth) {
+			this.#blocks.shift();
+		}
+		this.#updateNextPendingBlock(block);
+
+		return {
+			type: "record_new_block",
+			blockNumber: block.number,
+			blockHash: block.hash,
+			logsBloom: block.logsBloom,
+		};
+	}
+
+	/**
+	 * Create a new block watcher with the specified paramters.
+	 */
+	static async create({ client, lastIndexedBlock, ...config }: CreateParams) {
+		const self = new BlockWatcher(client, {
+			...DEFAULT_OPTIONS,
+			...config,
+		});
+		await self.#initialize(lastIndexedBlock);
+		return self;
+	}
+}
+
+const bmax = (a: bigint, b: bigint): bigint => (a > b ? a : b);
