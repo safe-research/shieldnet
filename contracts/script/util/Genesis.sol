39239df29209108b3bdb128d21dd96a610844bfe
Nicholas Rodrigues Lordello <nick@safe.dev>
2026-02-12 15:24:30 +0100

    Fix Genesis Group Computation

--- contracts/script/util/Genesis.sol
+++ contracts/script/util/Genesis.sol
@@ -15,20 +15,14 @@ library Genesis {
      * @return result The Merkle root hash.
      */
     function participantsMerkleRoot(address[] memory participants) internal pure returns (bytes32 result) {
-        uint256 depth = 0;
-        for (uint256 l = participants.length; l > 1; l = (l + 1) / 2) {
-            depth++;
-        }
-
-        // forge-lint: disable-next-line(incorrect-shift)
-        bytes32[] memory nodes = new bytes32[](1 << depth);
+        bytes32[] memory nodes = new bytes32[](participants.length);
         for (uint256 i = 0; i < participants.length; i++) {
             nodes[i] = keccak256(abi.encode(i + 1, participants[i]));
         }
-
-        for (uint256 w = nodes.length; w > 1; w /= 2) {
-            for (uint256 i = 0; i < w; i += 2) {
-                (bytes32 a, bytes32 b) = (nodes[i], nodes[i + 1]);
+        for (uint256 l = participants.length; l > 1; l = (l + 1) / 2) {
+            for (uint256 i = 0; i < l; i += 2) {
+                bytes32 a = nodes[i];
+                bytes32 b = i + 1 < l ? nodes[i + 1] : bytes32(0);
                 (bytes32 left, bytes32 right) = a < b ? (a, b) : (b, a);
                 nodes[i / 2] = keccak256(abi.encode(left, right));
             }
